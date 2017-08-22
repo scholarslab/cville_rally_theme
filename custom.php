19f5467d07ab3f35f75379588b1a32f298c82119
Jeremy Boggs <jeremyboggs@gmail.com>
2017-08-22 10:49:02 -0400

    Updates form field descriptions and labels.
    Refs #2.

--- custom.php
+++ custom.php
@@ -1,5 +1,32 @@
 <?php
 require_once dirname(__FILE__) . '/functions.php';
 
-add_filter(array('ElementForm', 'Item', 'Dublin Core', 'Description'), 'filter_contribution_description');
+add_filter(
+    array('ElementForm', 'Item', 'Dublin Core', 'Description'),
+    'filter_contribution_description'
+);
 
+add_filter(
+    array('ElementForm', 'Item', 'Dublin Core', 'Title'),
+    'filter_contribution_title'
+);
+
+add_filter(
+    array('ElementForm', 'Item', 'Item Type Metadata', 'Text'),
+    'filter_contribution_text'
+);
+
+add_filter(
+    array('ElementForm', 'Item', 'Dublin Core', 'Date'),
+    'filter_contribution_date'
+);
+
+add_filter(
+    array('ElementForm', 'Item', 'Dublin Core', 'Coverage'),
+    'filter_contribution_coverage'
+);
+
+add_filter(
+    array('ElementForm', 'Item', 'Dublin Core', 'Contributor'),
+    'filter_contribution_contributor'
+);
--- functions.php
+++ functions.php
@@ -2,6 +2,33 @@
 
 function filter_contribution_description($components) {
     $components['label'] = "Tell us about this deposit.";
-    $components['description'] = "Lorem ipsum dolor sit amet.";
+    $components['description'] = "Please include as much information as you can. For example: What was happening at this moment? Who is involved? What else do we need to know about this moment in time?";
     return $components;
 }
+
+function filter_contribution_title($components) {
+    $components['description'] = "For example, “Police in downtown Charlottesville,” “Difficult conversations,” or “What I witnessed from the Rotunda.”";
+    return $components;
+}
+
+function filter_contribution_text($components) {
+    $components['label'] = "Tell Your Story";
+    $components['description'] = "Please include as much information as you can. For example: What was happening at this moment? Who is involved? What else do we need to know about this moment in time?";
+    return $components;
+}
+
+function filter_contribution_date($components) {
+    $components['description'] = "When did this occur?";
+    return $components;
+}
+
+function filter_contribution_coverage($components) {
+    $components['description'] = "Where did this occur?";
+    return $components;
+}
+
+function filter_contribution_contributor($components) {
+    $components['description'] = '';
+    return $components;
+}
+
