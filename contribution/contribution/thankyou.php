e346404dedb67fea0a10c565623ab3c3d80eb927
Jeremy Boggs <jeremyboggs@gmail.com>
2017-08-25 11:36:41 -0400

    Syntax error.

--- contribution/contribution/thankyou.php
+++ contribution/contribution/thankyou.php
@@ -1,7 +1,7 @@
 <?php echo head(); ?>
 <div id="primary">
 	<h1><?php echo __("Thank you!"); ?></h1>
-	<p><?php echo __("Your submission was successful! Thank you. Feel free to %s." , contribution_link_to_contribute(__('make another contribution'));  ?>
+	<p><?php echo __("Your submission was successful! Thank you. Feel free to %s." , contribution_link_to_contribute(__('make another contribution')));  ?>
 	</p>
 </div>
 <?php echo foot(); ?>
