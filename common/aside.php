e2863d434ab501deb631324301a6d61bde3c9270
Jeremy Boggs <jeremyboggs@gmail.com>
2018-08-06 13:27:07 -0400

    Adds items, nav, to sidebar.

--- common/aside.php
+++ common/aside.php
@@ -3,7 +3,13 @@
   <div class="container">
     <div id="sidebar-area-top">
       <a href="<?php echo url('contribution'); ?>" id="contribution-button">Contribute Materials</a>
+        <a href="<?php echo url('items'); ?>" id="browse-items-nav">Browse the Collection</a>
+        <div id="recent-items">
+    <h2><?php echo __('Recent Items'); ?></h2>
+    <?php echo recent_items('3'); ?>
+</div><!--end recent-items -->
     </div>
+
   </div>
     <?php endif; ?>
 </aside><!-- /#sidebar -->
